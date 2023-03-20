20c695ede1cdcb39fb34ecbd97390696081e4255
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-03-20 23:12:31 +0900

    add point voucher endpoint

--- functions/src/controller/user.ts
+++ functions/src/controller/user.ts
@@ -1,16 +1,46 @@
-import {Request, Response} from "firebase-functions/v1";
+import { Request, Response } from 'firebase-functions/v1'
+import { ExtendRequest } from '../../types/http'
+import * as admin from 'firebase-admin'
 
-export const getMe = async (req: Request, res: Response) => {
-  res.json({id: 0, name: "taro"});
-  return;
-};
+const firestore = admin.firestore()
 
-export const signupUser = async (req: Request, res: Response) => {
-  res.json({});
-  return;
-};
+export const getMe = async (req: ExtendRequest, res: Response) => {
+  try {
+    if (!req.currentUser) {
+      res.sendStatus(403)
+      return
+    }
+    const user = (
+      await firestore.collection('users').doc(req.currentUser.sub).get()
+    ).data()
+
+    if (!user) {
+      res.sendStatus(404)
+    } else {
+      res.json(user)
+    }
+  } catch (error) {
+    res.sendStatus(403)
+  }
+  return
+}
+
+export const signupUser = async (req: ExtendRequest, res: Response) => {
+  try {
+    if (!req.currentUser) {
+      res.sendStatus(403)
+      return
+    }
+    const users = firestore.collection('users')
+    await users.doc(req.currentUser.sub).set({ name: req.currentUser.name })
+    res.sendStatus(200)
+  } catch (error) {
+    res.sendStatus(403)
+  }
+  return
+}
 
 export const deleteUser = async (req: Request, res: Response) => {
-  res.json({});
-  return;
-};
+  res.json({})
+  return
+}
