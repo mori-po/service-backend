2d392672f468bc41c3cbf0bee716533a46de2f89
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-03-25 14:12:24 +0900

    tmp

--- functions/types/voucher.ts
+++ functions/types/voucher.ts
@@ -10,4 +10,3 @@ export type Voucher = {
   point_amount: number
   expired_at: number
 }
-{"location_name":"東京駅","max_receivable_tickets":2,"event_date":1677423600,"event_description":"みんなでゴミ拾いをしよう！\nみんなで楽しもう！","max_supply":100,"event_name":"ゴミ拾い","event_image":"https://4.bp.blogspot.com/--EobH7fv_OQ/VVGVEMZ0III/AAAAAAAAthI/Lgt7o2KH5QE/s400/gomihiroi_boy.png","location":"35.680885, 139.769252","id":1,"expired_at":1680274800,"point_amount":100}
